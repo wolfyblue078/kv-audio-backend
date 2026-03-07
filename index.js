59f294e3f97dd871943e8352220c1855d1cd93e7
wolfyblue078 <wolfyblue078@gmail.com>
2026-03-07 10:23:51 +0530

    feat: database impement 🍑

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import e from "express";
+import mongoose from "mongoose";
 
 const app = e();
+
+
+//connect database
+const mongoURL = "mongodb+srv://admin:admin123@kv-audio.u1uy4xz.mongodb.net/?appName=kv-audio"
+mongoose.connect(mongoURL).then(()=>{
+    console.log("Database connect successfully!");
+}).catch((err)=>{
+    console.log(err.message);
+})
+
 
 app.listen(3000, ()=>{
     console.log("App is running on port 3000 ⏸️");
