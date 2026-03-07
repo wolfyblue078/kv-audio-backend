05ea5fa04540ba5025cc76165684cbe79910c70d
wolfyblue078 <wolfyblue078@gmail.com>
2026-03-07 10:45:00 +0530

    feat: create user registration

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+
+const registerUser = async (req, res)=>{
+
+    try {
+        const data = req.body;
+
+        const result = await User.findOne({email : data.email});
+        if(result){
+            return res.status(400).json({message : "User already exists"});
+        }
+
+
+        //password security
+        data.password = await bcrypt.hash(data.password, 10);
+        
+
+
+        const user = new User(data);
+        await user.save();
+        res.status(201).json({
+            message : "User created successfully",
+            "user details" : result
+        });
+    } catch (error) {
+        res.status(400).json({message : error.message});
+    }
+    
+}
