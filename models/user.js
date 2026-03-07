ce10c314857032ab8ad75bc7b7cfb8b6faa81778
wolfyblue078 <wolfyblue078@gmail.com>
2026-03-07 10:29:37 +0530

    feat: created user model

---
+++ models/user.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+
+const userSchema = mongoose.Schema({
+  firstName : {
+    type : String,
+    required : true,
+  },
+  lastName : {
+    type : String,
+    required : true,
+  },
+  email : {
+    type : String,
+    required : true,
+    unique : true
+  },
+  password : {
+    type : String,
+    required : true,
+  },
+  role: {
+    type : String,
+    required : true,
+    enum: ["user", "admin"],
+    default : "user"
+  },
+  address: {
+    type : String,
+    required : true,
+  },
+  phone : {
+    type : String,
+    required : true,
+  },
+  image : {
+    type : String,
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
