210c6d3bae8ef5542f7dc2efd95d6ea9c984f2ff
wolfyblue078 <wolfyblue078@gmail.com>
2026-03-21 10:35:41 +0530

    feat: add getProduct route to product router

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,10 @@
 import e from "express";
-import { addProduct } from "../controllers/productController.js";
+import { addProduct, getProduct } from "../controllers/productController.js";
 import { authMiddleware } from "../controllers/userController.js";
 
 let productRouter = e.Router();
 
 productRouter.post("/new",authMiddleware, addProduct);
-
+productRouter.get("/products", authMiddleware, getProduct);
 
 export default productRouter;
