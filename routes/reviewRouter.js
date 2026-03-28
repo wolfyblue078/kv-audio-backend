9d26c80b02bcbfcf167ede35041eff8a3ff4baaa
wolfyblue078 <wolfyblue078@gmail.com>
2026-03-28 10:48:25 +0530

    feat: add approveReview router

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,5 +1,5 @@
 import e from "express";
-import { addReview, deleteReview, getReviews } from "../controllers/reviewController.js";
+import { addReview, approveReview, deleteReview, getReviews } from "../controllers/reviewController.js";
 import { authMiddleware } from "../controllers/userController.js";
 
 const reviewRouter = e.Router();
@@ -7,5 +7,6 @@ const reviewRouter = e.Router();
 reviewRouter.post("/new",authMiddleware, addReview);
 reviewRouter.get("/reviews", authMiddleware, getReviews);
 reviewRouter.delete("/delete/:email",authMiddleware ,deleteReview);
+reviewRouter.put("/approve/:email", authMiddleware, approveReview);
 
 export default reviewRouter;
